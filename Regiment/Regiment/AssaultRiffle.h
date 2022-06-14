c87c25904bf38cce8d82f43a70b6d12e1c29134b
Plamena Stoyanova <93308618+pplamenass333@users.noreply.github.com>
2022-06-14 20:13:26 +0300

    Update AssaultRiffle.h

--- Regiment/Regiment/AssaultRiffle.h
+++ Regiment/Regiment/AssaultRiffle.h
@@ -1,12 +1,13 @@
 #pragma once
 #include "Weapons.h"
 
-const unsigned int MAX_AMMO_AR;
+const unsigned int MAX_AMMO_AR = 21000;
 class AssaultRiffle : public Weapons
 {
 	static unsigned ammoReserve;
 public:
-	AssaultRiffle(const float weight, const Condition cond, const unsigned int ammo);
+	AssaultRiffle();
+	AssaultRiffle(const float caliber, const unsigned int ammo);
 	
 	unsigned int getAmmoReserve()const;
 	unsigned int restockAmmo();
